849d53733bbd414e49f8f1c853e0b0653d67ec24
Kiran Purushothaman <projects@kirankp.com>
2023-01-30 08:42:59 -0800

    make sure expr string is not empty before calling lisp

--- frontend/scripts/cell_editor.gd
+++ frontend/scripts/cell_editor.gd
@@ -12,4 +12,5 @@ func _on_text_changed():
 	var c = self.get_caret_column()
 	if (c == 0):
 		var t = self.text.split("\n", false)
-		self.insert_text_at_caret(LispServer.eval(t[t.size() - 1]) + "\n")
+		if (t.size() > 0):
+			self.insert_text_at_caret(LispServer.eval(t[t.size() - 1]) + "\n")
