09eea48586526aa978ce73bac9cde3ed9a87851a
Kiran Purushothaman <projects@kirankp.com>
2023-10-15 10:51:36 -0700

    bidirectional comms using websocket

--- src/main.lisp
+++ src/main.lisp
@@ -9,6 +9,7 @@
 
 (defvar *clients* (make-hash-table))
 (defvar *server* nil)
+(defvar *websocket-server* nil)
 
 (defvar *current-client* nil)
 
@@ -18,8 +19,8 @@
                                                       4 "https://www.google.com/"
                                                       5 (1 2 3 4))))
 
-(setf yason:*symbol-encoder* #'yason:encode-symbol-as-lowercase
-      yason:*symbol-key-encoder* #'yason:encode-symbol-as-lowercase)
+(defun send-websocket-message (client msg)
+  (websocket-driver:send (slot-value client 'connection) msg))
 
 (defun handle-new-connection (connection)
   (log:info "Client connected!")
@@ -33,7 +34,8 @@
     (when (equal client *current-client*)
       (setf *current-client* nil))))
 
-(defun handle-message-received (connection msg-str)
+(defun handle-client-message (connection msg-str)
+  (declare (optimize (debug 3) (speed 0)))
   (log:info "Received message: ~a" msg-str)
   (let ((msg (with-input-from-string (s msg-str) (read s)))
         (client (gethash connection *clients*)))
@@ -41,12 +43,12 @@
       (:get-block (send-block-update client (getf msg :id)))
       (:set-block (setf (gethash (getf msg :id) *blocks*) (getf msg :value))))))
 
-(defun setup-mesha-server (env)
+(defun setup-websocket (env)
   (let ((ws (websocket-driver:make-server env)))
     (websocket-driver:on :open ws
                          (lambda () (handle-new-connection ws)))
     (websocket-driver:on :message ws
-                         (lambda (msg) (handle-message-received ws msg)))
+                         (lambda (msg) (handle-client-message ws msg)))
     (websocket-driver:on :error ws
                          (lambda (err) (log:error "~a" err)))
     (websocket-driver:on :close ws
@@ -58,14 +60,14 @@
       (websocket-driver:start-connection ws))))
 
 (defun send-message (client msg)
-  (websocket-driver:send (slot-value client 'connection) msg))
+  (websocket-driver:send-text (slot-value client 'connection) msg))
 
 (defun send-block-update (client block-id)
   (let* ((val (gethash block-id *blocks*))
          (message (alexandria:plist-hash-table
                    `(:operation "update-block"
                      :id ,block-id
-                     :value ,val))))
+                     :value ,(format nil "~a" val)))))
     (send-message client (yason:with-output-to-string* () (yason:encode message)))))
 
 (defun respond-ok (output &optional (output-type "text/html"))
@@ -98,11 +100,34 @@
       (:title "Mesha")
       (:meta :charset"UTF-8")
       (:meta :name "viewport" :content "width=device-width, initial-scale=1.0")
+      (:link :href "https://cdn.quilljs.com/1.3.6/quill.bubble.css" :rel "stylesheet")
       (:script :src "https://unpkg.com/htmx.org@1.9.6")
       (:script :src "https://unpkg.com/alpinejs" :defer "")
       (:script :src "https://cdn.tailwindcss.com")
       (:script :src "https://cdn.quilljs.com/1.3.6/quill.js")
-      (:link :href "https://cdn.quilljs.com/1.3.6/quill.bubble.css" :rel "stylesheet"))
+      (:script :src "https://cdnjs.cloudflare.com/ajax/libs/mousetrap/1.6.5/mousetrap.min.js")
+      (:script
+       (:raw
+        (parenscript:ps
+          (defvar *rich-text-editor* nil)
+          (defvar *websocket* nil)
+
+          (defun handle-server-message (msg-event)
+            (let* ((obj (parenscript:chain +JSON+ (parse (parenscript:@ msg-event data))))
+                   (val (parenscript:@ obj value)))
+              (when (parenscript:stringp val)
+                (parenscript:chain *rich-text-editor* (set-text val))))
+            nil)
+          
+          (defun init-mesha-client ()
+            ;; (parenscript:chain -Mousetrap (bind "ctrl+enter" (lambda () (parenscript:chain *rich-text-editor* (focus)))))
+            (setf *websocket* (parenscript:new (-Web-Socket "ws://localhost:13330")))
+            (setf (parenscript:@ *websocket* onopen) (lambda (open-event)
+                                                       (setf (parenscript:@ *websocket* onmessage) #'handle-server-message)
+                                                       nil))
+            (when (equal *rich-text-editor* nil)
+              (setf *rich-text-editor* (parenscript:new (-Quill "#editor-container" (parenscript:create theme "bubble")))))
+            nil)))))
      (:body :class "bg-gray-300 dark:bg-gray-700"
             (:div :class "flex flex-col h-screen"
                   (:nav :class "bg-gray-800"
@@ -118,36 +143,46 @@
                   (:main :class "h-full"
                          (:div :class "w-full h-full grow flex px-6 py-6"
                                (:div :id "content" :class "bg-gray-600 w-full px-6 py-6"
-                                     (:p "Hello there")
-                                     (:br)
                                      (:button :attrs *button* :hx-post "/clicked" :hx-swap "innerHTML" "Click Me")
                                      (:br)
                                      (:br)
-                                     (:div :class "rounded-xl bg-gray-700 shadow-lg p-5 text-white"
-                                           :x-data "new Quill('#editor-container', {theme: 'bubble'})"
-                                           (:div :id "editor-container" :class "max-h-full"))))))))))
+                                     (:div :class "rounded-xl bg-gray-200 shadow-lg p-5"
+                                           (:div :id "editor-container" :x-init (parenscript:ps (init-mesha-client)) :class "max-h-full"))))))))))
+
+(defparameter *button-state* nil)
 
 (defun handler (env)
   (declare (optimize (debug 3) (speed 0)))
   (let ((path (getf env :path-info)))
     (match path
       ("/" (respond-ok *main-view*))
-      ("/hello" (respond-ok (with-html-string (:h1 "Test a route"))))
-      ("/clicked" (respond-ok (with-html-string (:h1 "HTMX Works"))))
+      ("/clicked" (respond-ok (progn
+                                (setf *button-state* (not *button-state*))
+                                (if *button-state*
+                                    (with-html-string (:div :x-init (parenscript:ps (parenscript:chain *websocket* (send "(:operation :get-block :id 1)")))
+                                                            "I've been clicked"))
+                                    (with-html-string (:div :x-init (parenscript:ps (parenscript:chain *websocket* (send "(:operation :get-block :id 2)"))) "Click me"))))))
       (_ (respond-404 (with-html-string (:div (:h1 "Error 404")
                                               (:p "Content not found"))))))))
 
 (defun main ()
   (log:info "Starting mesha server")
+  
+  (setf yason:*symbol-encoder* #'yason:encode-symbol-as-lowercase
+        yason:*symbol-key-encoder* #'yason:encode-symbol-as-lowercase)
+
   ;; ignore htmx attributes
   (pushnew "hx-" *unvalidated-attribute-prefixes* :test #'equal)
   ;; ignore alpine.js attributes
   (pushnew "x-" *unvalidated-attribute-prefixes* :test #'equal)
   (pushnew "@" *unvalidated-attribute-prefixes* :test #'equal)
-  (setf *server*
-        (clack:clackup (lambda (env) (funcall 'handler env))
-                       :address "0.0.0.0"
-                       :port 13333)))
+
+  (setf *server* (clack:clackup (lambda (env) (funcall 'handler env))
+                                ;; :address "0.0.0.0"
+                                :port 13333)
+        *websocket-server* (clack:clackup (lambda (env) (funcall 'setup-websocket env))
+                                          ;; :address "0.0.0.0"
+                                          :port 13330)))
 
 (defun shutdown ()
   (clack:stop *server*)
