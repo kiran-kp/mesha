a55c786b5ca07fbb1c7f657a18cdae39931b9284
Kiran Purushothaman <projects@kirankp.com>
2024-07-04 22:56:39 -0700

    readd cffi setup to loader

--- loader.lisp
+++ loader.lisp
@@ -1,4 +1,6 @@
 (uiop:chdir "/home/kiran/projects/mesha")
+(ql:quickload "cffi")
+(pushnew #P"/home/kiran/projects/mesha/build/" cffi:*foreign-library-directories*)
 (pushnew #P"/home/kiran/projects/mesha/extern/" asdf:*central-registry*)
 (ql:quickload "mesha")
 
