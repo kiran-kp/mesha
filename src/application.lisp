e981ea6ec763171a50cb373b81c55db316a15f64
Kiran Purushothaman <projects@kirankp.com>
2024-07-20 23:57:44 -0700

    use a "command buffer" to send draw commands to client
    removed sdl references

--- src/application.lisp
+++ src/application.lisp
@@ -11,50 +11,10 @@
    (fonts :initform (make-hash-table))
    (exiting :initform nil)))
 
-#+nil
-(defun application-run (app init-fn update-fn render-fn)
-  (v:info :application "Starting application loop")
-  (let ((view (get-viewport app)))
-    (sdl2:with-init (:everything)
-      (let ((ctx (mesha-native:create-context)))
-        (assert ctx)
-        (setf (slot-value app 'context) ctx))
-      (funcall init-fn app)
-      (sdl2:with-window (win :title "Mesha" :flags '(:shown) :w (round (vx view)) :h (round (vy view)))
-        (sdl2:with-renderer (renderer win :flags '(:accelerated :presentvsync))
-          (sdl2:with-event-loop (:method :poll)
-            (:idle
-             ()
-             (sdl2:set-render-draw-color renderer 0 0 0 255)
-             (sdl2:render-clear renderer)
-             (funcall update-fn app)
-             (funcall render-fn app renderer)
-             (sdl2:render-present renderer))
-            (:quit
-             ()
-             ()
-             (v:info :application "Exiting event loop")
-             t)))))))
-
 (defparameter *rect-x* 1)
 (defparameter *rect-y* 1)
 (defparameter *move-x* 1)
 (defparameter *move-y* 1)
-
-(defun render (body canvas ctx)
-  (clog:clear-rect ctx 0 0 (clog:width canvas) (clog:height canvas))
-  (setf (clog:stroke-style ctx) :black)
-
-  (incf *rect-x* (* 5 *move-x*))
-  (incf *rect-y* (* 5 *move-y*))
-
-  (unless (< 0 *rect-x* 1820)
-    (setf *move-x* (* -1 *move-x*)))
-  (unless (< 0 *rect-y* 1000)
-    (setf *move-y* (* -1 *move-y*)))
-
-  (setf (clog:fill-style ctx) :red)
-  (clog:fill-rect ctx *rect-x* *rect-y* 100 50))
 
 (defun enqueue-command (command)
   (with-slots (commands) *application*
@@ -74,30 +34,24 @@
            (with-slots (document) app
              (setf document (make-table 3 5 nil))))
           (:draw-cells
-           (with-slots (client) app
-             (let ((y-start 10)
-                   (x 10)
-                   (y 10))
-               (clog:js-execute client "clear_cells();")
-               (table-iterate (slot-value *application* 'document)
-                              (lambda (col width)
-                                (column-iterate col
-                                                (lambda (width height content)
-                                                  (declare (ignore content)
-                                                           (optimize (debug 3) (speed 0)))
-                                                  (clog:js-execute client
-                                                                   (format nil
-                                                                           "mesha.cells.push({ rect: new PIXI.Rectangle(~a, ~a, ~a, ~a) });"
-                                                                           x
-                                                                           y
-                                                                           width
-                                                                           height))
-                                                  (incf y height)))
-                                (incf x width)
-                                (setf y y-start)))
-               (clog:js-execute client "redraw_cells();"))))
+           (with-output-to-string (cmds)
+             (flet ((push-rect (x y w h content) (format cmds "mesha.cells.push({ rect: new PIXI.Rectangle(~a, ~a, ~a, ~a), content: \"~a\" });" x y w h content)))
+               (with-slots (client) app
+                 (let ((y-start 10)
+                       (x 10)
+                       (y 10))
+                   (format cmds "clear_cells();")
+                   (table-iterate (slot-value *application* 'document)
+                                  (lambda (col width)
+                                    (column-iterate col
+                                                    (lambda (width height content)
+                                                      (push-rect x y width height content)
+                                                      (incf y height)))
+                                    (incf x width)
+                                    (setf y y-start)))
+                   (format cmds "redraw_cells();")
+                   (clog:js-execute client (get-output-stream-string cmds)))))))
           (:set-cell-text
-           #+nil
            (let ((c (gethash (second cmd) *cells*))
                  (text (third cmd)))
              (setf (slot-value c 'content) text
@@ -106,7 +60,8 @@
 
 (defun on-new-window (body)
   (unless *application*
-    (setf *application* (make-instance 'application)))
+    (setf *application* (make-instance 'application))
+    (enqueue-command '(:make-table)))
   (setf (clog:connection-data-item body "app-data") *application*)
   (clog:load-script (clog:html-document body) "https://pixijs.download/release/pixi.js")
   (clog:load-script (clog:html-document body) "js/mesha.js")
@@ -114,6 +69,7 @@
   (clog-gui:clog-gui-initialize body)
 
   (setf (slot-value *application* 'client) body)
+  (enqueue-command '(:draw-cells))
   (loop while (not (slot-value *application* 'exiting))
         do (process-commands *application*)))
 
--- src/main.lisp
+++ src/main.lisp
@@ -5,28 +5,12 @@
 (define-condition unknown-command-error (error)
   ((command :initarg :command :reader command)))
 
-
 (defun update (app)
   (declare (optimize (debug 3) (speed 0)))
   (process-commands app))
 
 (defun point-in-rectp (point r)
   (v<= (vxy r) point (v+ (vxy r) (vzw r))))
-
-(defun render (app renderer)
-  (declare (optimize (speed 0) (debug 3)))
-  (let ((rects (table-render (slot-value app 'document)))
-        (box (sdl2:make-rect 0 0 0 0)))
-    (multiple-value-bind (mx my) (sdl2:mouse-state)
-      (loop for r in rects
-            do (setf (sdl2:rect-x box) (round (vx r))
-                     (sdl2:rect-y box) (round (vy r))
-                     (sdl2:rect-width box) (round (vz r))
-                     (sdl2:rect-height box) (round (vw r)))
-               (if (point-in-rectp (vec mx my) r)
-                 (sdl2:set-render-draw-color renderer 0 0 255 255)
-                 (sdl2:set-render-draw-color renderer 0 255 0 255))
-               (sdl2:render-draw-rect renderer box)))))
 
 (defun init (app)
   (v:info :application "Queuing startup commands")
