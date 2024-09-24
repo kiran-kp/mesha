79c64458c789f6d52f927b0a45a5a57dc839f01e
Kiran Purushothaman <projects@kirankp.com>
2024-09-23 22:47:52 -0700

    fix formatting

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,29 @@
 // Third Party
 #include <SDL2/SDL.h> // For Mac, use <SDL.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
     // Create a window data type
     // This pointer will point to the 
     // window that is allocated from SDL_CreateWindow
-    SDL_Window* window=nullptr;
+    SDL_Window* window = nullptr;
 
     // Initialize the video subsystem.
     // iF it returns less than 1, then an
     // error code will be received.
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+    if (SDL_Init(SDL_INIT_VIDEO) < 0){
         std::cout << "SDL could not be initialized: " <<
-                  SDL_GetError();
-    }else{
+            SDL_GetError();
+    } else {
         std::cout << "SDL video system is ready to go\n";
     }
+
     // Request a window to be created for our platform
     // The parameters are for the title, x and y position,
     // and the width and height of the window.
-    window = SDL_CreateWindow("C++ SDL2 Window",20, 20, 640,480,SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Mesha", 20, 20, 640, 480, SDL_WINDOW_SHOWN);
 
     SDL_Renderer* renderer = nullptr;
-    renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     
     // Create a rectangle
     SDL_Rect rectangle;
@@ -40,47 +41,29 @@ int main(int argc, char* argv[]){
     rectangle.w = 20;
     rectangle.h = 20;
 
-
-    // Infinite loop for our application
     bool gameIsRunning = true;
-    // Main application loop
-    while(gameIsRunning){
+    while(gameIsRunning) {
         SDL_Event event;
 
-        // (1) Handle Input
-        // Start our event loop
-        while(SDL_PollEvent(&event)){
-            // Handle each specific event
-            if(event.type == SDL_QUIT){
-                gameIsRunning= false;
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                gameIsRunning = false;
             }
         }
-        // (2) Handle Updates
-        
-        // (3) Clear and Draw the Screen
-        // Gives us a clear "canvas"
-        SDL_SetRenderDrawColor(renderer,0,0,0,SDL_ALPHA_OPAQUE);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
 
-        // Do our drawing
-        SDL_SetRenderDrawColor(renderer,255,255,255,SDL_ALPHA_OPAQUE);
-        SDL_RenderDrawLine(renderer,5,5,100,120);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawLine(renderer, 5, 5, 100, 120);
 
-        SDL_RenderDrawRect(renderer,&rectangle);
+        SDL_RenderDrawRect(renderer, &rectangle);
 
-
-        // Finally show what we've drawn
         SDL_RenderPresent(renderer);
 
     }
 
-    // We destroy our window. We are passing in the pointer
-    // that points to the memory allocated by the 
-    // 'SDL_CreateWindow' function. Remember, this is
-    // a 'C-style' API, we don't have destructors.
     SDL_DestroyWindow(window);
     
-    // our program.
     SDL_Quit();
     return 0;
 }
