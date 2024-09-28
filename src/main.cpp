95f55f2cc35a422c53c7028242a9de3a8438e92f
Kiran Purushothaman <kiran@Kirans-Laptop.local>
2024-09-27 23:10:47 -0700

    load a script file and run "main"

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #include <ui.h>
 
 #include <SDL.h>
@@ -10,11 +8,60 @@
 
 #include <janet.h>
 
-int main(int, char**) {
+#include <fstream>
+#include <string>
+
+auto slurp(std::string_view path) -> std::string {
+    constexpr auto read_size = std::size_t(4096);
+    auto stream = std::ifstream(path.data());
+    stream.exceptions(std::ios_base::badbit);
+
+    if (!stream) {
+        throw std::ios_base::failure("File does not exist");
+    }
+    
+    auto out = std::string();
+    auto buf = std::string(read_size, '\0');
+    while (stream.read(& buf[0], read_size)) {
+        out.append(buf, 0, stream.gcount());
+    }
+
+    out.append(buf, 0, stream.gcount());
+    return out;
+}
+
+auto main(int argc, char **argv) -> int {
     janet_init();
     JanetTable *env = janet_core_env(NULL);
+    JanetArray *args;
 
-    janet_dostring(env, "(print \"hello, world!\")", "main", NULL);
+    /* Create args tuple */
+    args = janet_array(argc);
+    for (int i = 1; i < argc; i++) {
+        janet_array_push(args, janet_cstringv(argv[i]));
+    }
+
+    /* Save current executable path to (dyn :executable) */
+    janet_table_put(env, janet_ckeywordv("executable"), janet_cstringv(argv[0]));
+
+    auto boot_file = slurp("../src/mesha.janet");
+    Janet output;
+    janet_dobytes(env,
+                  reinterpret_cast<const uint8_t*>(boot_file.c_str()),
+                  static_cast<int32_t>(boot_file.length()),
+                  "../src/mesha.janet",
+                  &output);
+
+    /* Run startup script */
+    Janet mainfun;
+    janet_resolve(env, janet_csymbol("main"), &mainfun);
+    Janet mainargs[1] = { janet_wrap_array(args) };
+    JanetFiber *fiber = janet_fiber(janet_unwrap_function(mainfun), 64, 1, mainargs);
+    janet_gcroot(janet_wrap_fiber(fiber));
+    fiber->env = env;
+
+    /* Run the fiber in an event loop */
+    auto status = janet_loop_fiber(fiber);
 
     Ui ui;
     if (mesha_ui_init(ui)) {
---
+++ src/mesha.janet
@@ -0,0 +1,7 @@
+# Application code for Mesha
+
+(defn main
+  [args]
+  
+  (setdyn *args* args)
+  (print "Hello world from scripts!"))
