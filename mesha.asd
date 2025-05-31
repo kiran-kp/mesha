0bed4302359c19bf3eee06f9f1b2b59a5fff2b56
Kiran Purushothaman <projects@kirankp.com>
2025-05-31 14:57:58 -0700

    Use SDL3 from ocicl

--- mesha.asd
+++ mesha.asd
@@ -4,28 +4,30 @@
 ;; (add-to-list 'load-path (expand-file-name "~/projects/mesha/systems/sly-20250207-c48defcf"))
 ;; (load-file (expand-file-name "~/projects/mesha/systems/sly-20250207-c48defcf/sly.el"))
 
+;; (asdf:load-system :mesha)
+
 (asdf:defsystem #:mesha
-    :description "A productivity tool"
-    :author "Kiran <projects@kirankp.com>"
-    :license  "MIT"
-    :version "0.0.1"
-    :serial t
-    :pathname "src/"
-    :build-operation "program-op"
-    :build-pathname "../mesha"
-    :entry-point "mesha:main"
-    :components ((:file "sdl3")
-                 (:file "package")
-                 (:file "main"))
-    :depends-on (#:alexandria
-                 #:serapeum
-                 #:cffi
-                 #:3d-vectors
-                 #:bordeaux-threads
-                 #:str
-                 #:trivia
-                 #:cl-dbi
-                 #:frugal-uuid
-                 #:sxql
-                 #:trivial-main-thread
-                 #:slynk))
+  :description "A productivity tool"
+  :author "Kiran <projects@kirankp.com>"
+  :license  "MIT"
+  :version "0.0.1"
+  :serial t
+  :pathname "src/"
+  :build-operation "program-op"
+  :build-pathname "../mesha"
+  :entry-point "mesha:main"
+  :components ((:file "package")
+               (:file "main"))
+  :depends-on (#:alexandria
+               #:serapeum
+               #:cffi
+               #:3d-vectors
+               #:bordeaux-threads
+               #:str
+               #:trivia
+               #:cl-dbi
+               #:frugal-uuid
+               #:sxql
+               #:trivial-main-thread
+               #:slynk
+               #:sdl3))
--- src/main.lisp
+++ src/main.lisp
@@ -130,20 +130,243 @@ when `end` is updated in ring-buffer-push"))
                             :type (getf row :|mtype|)
                             :content (getf row :|mcontent|)))))))
 
+(defparameter *step-rate-in-ms* 125)
+(defparameter *snake-block-size-in-pixels* 48)
+(defparameter *snake-game-width* 10)
+(defparameter *snake-game-height* 10)
 
-(defmacro run-in-gui-thread (&body f)
-  `(tmt:with-body-in-main-thread (:blocking t)
-     (progn ,@f)))
+(deftype cell-value ()
+  `(member :empty :food :left :right :up :down))
 
-(defun init-sdl ()
-  (run-in-gui-thread
-    (sdl3:init :video)
-    (let ((window (sdl::create-window "Test" 800 600 :high-pixel-density)))
-      (sdl::flash-window window :until-focused))))
+(defclass game-state ()
+  ((cells :accessor cells)
+   (head-pos :accessor head-pos)
+   (tail-pos :accessor tail-pos)
+   (next-dir :accessor next-dir)
+   (occupied-cells :initform 0 :accessor occupied-cells)))
+
+(defun cell (cells pos)
+  (aref cells (first pos) (second pos)))
+
+(defun (setf cell) (newval cells pos)
+  (setf (aref cells (first pos) (second pos)) newval))
+
+
+(defun place-food (state)
+  (with-slots (occupied-cells cells) state
+    (unless (= occupied-cells (* *snake-game-height* *snake-game-width*))
+      (loop for x = (random *snake-game-width*)
+            for y = (random *snake-game-height*)
+            when (eql (aref cells x y) :empty)
+              do (return (setf (aref cells x y) :food))))))
+
+(defun initialize-game-state (state)
+  (with-slots (cells head-pos tail-pos next-dir occupied-cells) state
+    (let* ((mid-x (floor *snake-game-width* 2))
+           (mid-y (floor *snake-game-height* 2)))
+      (setf head-pos (list mid-x mid-y)
+            tail-pos (list (- mid-x 3) mid-y)
+            next-dir :right
+            cells (make-array (list *snake-game-width* *snake-game-height*)
+                              :initial-element :empty
+                              :element-type 'cell-value)
+            occupied-cells 4)
+      (loop for x from mid-x downto (- mid-x 3) do
+        (setf (aref cells x mid-y) :right))
+      (loop repeat 4 do (place-food state)))
+    state))
+
+(defun next-position (pos dir)
+  (destructuring-bind (x y) pos
+    (ecase dir
+      (:right (values (mod (+ x 1) *snake-game-width*) y))
+      (:left (values (mod (- x 1) *snake-game-width*) y))
+      (:up (values x (mod (- y 1) *snake-game-height*)))
+      (:down (values x (mod (+ y 1) *snake-game-height*))))))
+
+(defun opposite-dir (dir)
+  (ecase dir
+    (:up :down)
+    (:down :up)
+    (:left :right)
+    (:right :left)))
+
+(defun game-step (state)
+  (declare (optimize (debug 3)))
+  (with-slots (cells head-pos tail-pos next-dir occupied-cells) state
+    (when (eql (cell cells head-pos) (opposite-dir next-dir))
+      (setf next-dir (cell cells head-pos)))
+    (multiple-value-bind (x y) (next-position head-pos next-dir)
+      (flet ((move-head ()
+               (setf (aref cells (first head-pos) (second head-pos)) next-dir)
+               (setf (first head-pos) x
+                     (second head-pos) y)
+               (setf (aref cells x y) next-dir))
+
+             (move-tail ()
+               (let ((tail-dir (cell cells tail-pos)))
+                 (setf (cell cells tail-pos) :empty)
+                 (multiple-value-bind (x y)
+                     (next-position tail-pos tail-dir)
+                   (setf (first tail-pos) x
+                         (second tail-pos) y)))))
+
+        (case (aref cells x y)
+          (:empty
+           (move-head)
+           (move-tail))
+          (:food
+           (move-head)
+           (place-food state)
+           (incf occupied-cells))
+          (t ;; collision with self
+           (initialize-game-state state)))))))
+
+(defclass app ()
+  ((window :initarg :window :accessor window)
+   (renderer :initarg :renderer :accessor renderer)
+   (state :initarg :state :accessor state)
+   (last-step :initarg :last-step :accessor last-step)))
+
+(defparameter *app* nil)
+
+(defun assert-ret (return-value)
+  (when (null return-value)
+    (error "Error: ~a" (sdl3:get-error))))
+
+(defun init ()
+  (assert-ret (sdl3:set-app-metadata "Example Snake game" "1.0" "com.example.Snake"))
+  (loop for (key value) on (list sdl3:+app-url-string+  "https://examples.libsdl.org/SDL3/demo/01-snake/"
+                                 sdl3:+app-creator-string+  "SDL team"
+                                 sdl3:+app-copyright-string+  "Placed in the public domain"
+                                 sdl3:+app-type-string+  "game")
+          by #'cddr do
+            (assert-ret (sdl3:set-app-metadata-property key value)))
+
+  (assert-ret (sdl3:init :video))
+
+  ;; Destroy old windows
+  ;; Useful during development where old crashed windows need cleanup
+  (loop for window in (sdl3:get-windows)
+        for renderer = (sdl3:get-renderer window)
+        unless (or (cffi:null-pointer-p window)
+                   (cffi:null-pointer-p renderer))
+          do (assert-ret (sdl3:destroy-renderer renderer))
+             (assert-ret (sdl3:destroy-window window)))
+
+  (multiple-value-bind (ret window renderer)
+      (sdl3:create-window-and-renderer
+       "test/demo/snake"
+       (* *snake-block-size-in-pixels* *snake-game-width*)
+       (* *snake-block-size-in-pixels* *snake-game-height*)
+       nil)
+    (assert-ret ret)
+
+    (setf *app* (make-instance 'app
+                               :window window
+                               :renderer renderer
+                               :state (initialize-game-state (make-instance 'game-state))
+                               :last-step (sdl3:get-ticks))))
+  :continue)
+
+(defun handle-event (event)
+  (typecase event
+    (sdl3:quit-event :success)
+    (sdl3:keyboard-event
+     (let* ((key (slot-value event 'sdl3:%key))
+            (scancode (slot-value event 'sdl3:%scancode)))
+       (when (member key '(:up :down :left :right))
+         (setf (next-dir (state *app*)) key))
+       (if (eql key :q)
+           :success
+           :continue)))
+    (t :continue)))
+
+(defun iterate ()
+  (let ((now (sdl3:get-ticks))
+        (rect (make-instance 'sdl3:frect
+                             :%h (coerce *snake-block-size-in-pixels* 'float)
+                             :%w (coerce *snake-block-size-in-pixels* 'float))))
+    (with-slots (renderer last-step state) *app*
+      ;; Run physics updates
+      (loop while (> (- now last-step)
+                     *step-rate-in-ms*)
+            do
+               (incf last-step *step-rate-in-ms*)
+               (game-step state))
+
+      ;; Sleep until UI update time
+      (when (< (- now last-step) *step-rate-in-ms*)
+        (sleep (/ (- now last-step) 1000 5)))
+
+      ;; Render game board
+      (sdl3:set-render-draw-color renderer 0 0 0 255)
+      (sdl3:render-clear renderer)
+
+      (flet ((fill-rect (x y r g b)
+               (sdl3:set-render-draw-color renderer r g b 255)
+               (setf (slot-value rect 'sdl3:%x) (* 1.0 x *snake-block-size-in-pixels*)
+                     (slot-value rect 'sdl3:%y) (* 1.0 y *snake-block-size-in-pixels*))
+               (sdl3:render-fill-rect renderer rect)))
+        (loop for x from 0 below *snake-game-width* do
+          (loop for y from 0 below *snake-game-height*
+                for cell = (aref (cells state) x y) do
+                  (case cell
+                    (:empty
+                     t)
+                    (:food
+                     (fill-rect x y 80 80 255))
+                    (t
+                     (fill-rect x y 0 128 0)))))
+
+        (destructuring-bind (x y) (head-pos state)
+          (fill-rect x y 255 255 0))
+        (sdl3:render-present renderer)))
+    :continue))
+
+(defun handle-quit ()
+  (sdl3:destroy-renderer (renderer *app*))
+  (sdl3:destroy-window (window *app*))
+  (sdl3:pump-events)
+  (sdl3:quit-sub-system :video)
+  (sdl3:quit))
+
+;;;;; Example of how to run app by writing you own event and ui loop
+(defun run-snake% ()
+  (init)
+  (unwind-protect
+       (loop named outer do
+         (loop named event-loop
+               for event = (sdl3:poll-event*)
+             while event
+             do
+                (let* ((next (handle-event event)))
+                  (unless (eql next :continue)
+                    (return-from outer))))
+         (iterate))
+    (handle-quit)))
+
+(sdl3:def-app-init demo-init (argc argv)
+  (declare (ignore argc argv))
+  (init))
+
+(sdl3:def-app-iterate demo-iterate ()
+  (iterate))
+
+(sdl3:def-app-event demo-event (type event)
+  (declare (ignorable type))
+  (handle-event (sdl3:event-unmarshal event)))
+
+(sdl3:def-app-quit demo-quit (result)
+  (declare (ignore result))
+  (handle-quit))
 
 (defun main ()
-  (format t "Mesha init")
-  (setf *db*
-        (dbi:connect :sqlite3
-                     :database-name (asdf:system-relative-pathname 'mesha "notes.mesha")))
-  (init-sdl))
+  (sdl3:enter-app-main-callbacks 'demo-init 'demo-iterate 'demo-event 'demo-quit))
+
+;; (defun main ()
+;;   (format t "Mesha init")
+;;   (setf *db*
+;;         (dbi:connect :sqlite3
+;;                      :database-name (asdf:system-relative-pathname 'mesha "notes.mesha")))
+;;   (init-sdl))
